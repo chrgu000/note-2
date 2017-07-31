6573f08f9ab1f64e674e899e5cce2da6ca991e05
liyunfengfengfeng <2305743208@qq.com>
2017-07-31 11:20:51 +0800

    commit by liyunfengfengfeng

---
+++ src/com/company/meituan/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.company.meituan;
+
+/**
+ * Created by a1996_000 on 2017/7/31.
+ */
+
+/**
+ *我的第一个java程序
+ */
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/company/meituan/a.java
@@ -0,0 +1,18 @@
+package com.company.meituan;
+
+/**
+ * Created by a1996_000 on 2017/7/31.
+ */
+public class a {
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    private int num;
+    public a(){}
+    public a(int a){this.num = a;}
+}
